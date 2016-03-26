6b79467beee55a1cf4885f53ef4ed4436945800c
Marco Craveiro <marco.craveiro@gmail.com>
2016-03-26 17:33:59 +0800

    w3m: add some useful settings

--- personal/init-w3m.el
+++ personal/init-w3m.el
@@ -33,9 +33,13 @@
 ;;; Code:
 (prelude-require-package 'w3m)
 
-
 ;; change default browser for 'browse-url'  to w3m
 (setq browse-url-browser-function 'w3m-goto-url-new-session)
+
+; (setq browse-url-browser-function 'w3m-browse-url)
+; (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+;; optional keyboard short-cut
+; (global-set-key "\C-xm" 'browse-url-at-point)
 
 ;;change w3m user-agent to android
 (setq w3m-user-agent "Mozilla/5.0 (Linux; U; Android 2.3.3; zh-tw; HTC_Pyramid Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.")
