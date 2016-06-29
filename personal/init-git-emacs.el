45db140a78a0c5e59cd454f2f06ab68b12f0b8f8
marco <marco.craveiro@gmail.com>
2016-06-29 15:39:26 +0100

    magit: show status in current buffer

--- personal/init-git-emacs.el
+++ personal/init-git-emacs.el
@@ -41,4 +41,16 @@
 (setq magit-auto-revert-mode nil)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+(setq magit-display-buffer-function
+      (lambda (buffer)
+        (display-buffer
+         buffer (if (and (derived-mode-p 'magit-mode)
+                         (memq (with-current-buffer buffer major-mode)
+                               '(magit-process-mode
+                                 magit-revision-mode
+                                 magit-diff-mode
+                                 magit-stash-mode
+                                 magit-status-mode)))
+                    nil
+                  '(display-buffer-same-window)))))
 ;;; init-git-emacs.el ends here
